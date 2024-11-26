c3b81cf9cae766503336d5c004ad6ba1ee63135f
Alex Richert <alexander.richert@noaa.gov>
2024-11-25 20:27:10 -0800

    extend gbyte{,c,sc} testing

--- tests/test_gbytec.F90
+++ tests/test_gbytec.F90
@@ -27,7 +27,7 @@ program test_gbytec
   integer :: iskip = 0
   integer :: nbits = 8
   integer :: nskip = 0
-  integer :: i
+  integer :: i, iter
   integer :: num
   
   print *, 'Testing sbyte subroutines.'
@@ -37,29 +37,41 @@ program test_gbytec
   out(1) = char(0)
   call sbytec(out, in, iskip, nbits)
   if (ichar(out(1)) .ne. in(1)) stop 10
+  out(1) = char(0)
+  call gbytec(out, in, iskip, nbits)
+  if (ichar(out(1)) .ne. in(1)) stop 11
 
   print *, 'Testing sbytesc()...'
   in(1) = 3
   out(1) = char(0)
   call sbytesc(out, in, iskip, nbits, nskip, n)
   if (ichar(out(1)) .ne. in(1)) stop 20
+  out(1) = char(0)
+  call gbytesc(out, in, iskip, nbits, nskip, n)
+  if (ichar(out(1)) .ne. in(1)) stop 21
 
   ! THIS will pack the numbers 1 and 2 into the first two chars of the
   ! buffer. The rest of the output buffer will remain zeros.
   print *, 'Testing g2_sbytesc() packing 2 values...'
   in2(1) = 1
   in2(2) = 2
-  do i = 1, 8
-     out8(i) = char(0)
-  end do
   nbits = 8
-  call sbytesc(out8, in2, iskip, nbits, nskip, n2)
-  do i = 1, 8
-     if (i .le. 2) then
-        if (ichar(out8(i)) .ne. in2(i)) stop 30;
-     else
-        if (ichar(out8(i)) .ne. 0) stop 31;
-     endif
+  do iter = 1, 2
+    do i = 1, 8
+       out8(i) = char(0)
+    end do
+    if (iter .eq. 1) then
+      call sbytesc(out8, in2, iskip, nbits, nskip, n2)
+    else
+      call gbytesc(out8, in2, iskip, nbits, nskip, n2)
+    end if
+    do i = 1, 8
+       if (i .le. 2) then
+          if (ichar(out8(i)) .ne. in2(i)) stop 30;
+       else
+          if (ichar(out8(i)) .ne. 0) stop 31;
+       endif
+    end do
   end do
 
   ! Now pack 5 values into the 5 character array out5.
@@ -71,12 +83,18 @@ program test_gbytec
   in5(5) = 5
   nbits = 8
   nskip = 0
-  do i = 1, 5
-     out5(i) = char(0)
-  end do
-  call sbytesc(out5, in5, iskip, nbits, nskip, n5)
-  do i = 1, 5
-     if (ichar(out5(i)) .ne. in5(i)) stop 40;     
+  do iter = 1, 2
+    do i = 1, 5
+       out5(i) = char(0)
+    end do
+    if (iter .eq. 1) then
+      call sbytesc(out5, in5, iskip, nbits, nskip, n5)
+    else
+      call gbytesc(out5, in5, iskip, nbits, nskip, n5)
+    end if
+    do i = 1, 5
+       if (ichar(out5(i)) .ne. in5(i)) stop 40;     
+    end do
   end do
 
   ! Now pack 5 values into the 10 character array out10. Skip every
@@ -84,17 +102,23 @@ program test_gbytec
   print *, 'Testing g2_sbytesc() packing 5 values, skipping every other byte...'
   nbits = 8
   nskip = 0
-  do i = 1, 10
-     out10(i) = char(0)
-  end do
-  call sbytesc(out10, in5, iskip, nbits, 8, 5)
-  do i = 1, 10
-     ! print '(z2.2)', out10(i)
-     if (mod(i, 2) .gt. 0) then
-        if (ichar(out10(i)) .ne. in5(int(i/2) + 1)) stop 51;
-     else
-        if (ichar(out10(i)) .ne. 0) stop 50;
-     endif
+  do iter = 1, 2
+    do i = 1, 10
+       out10(i) = char(0)
+    end do
+    if (iter .eq. 1) then
+      call sbytesc(out10, in5, iskip, nbits, 8, 5)
+    else
+      call gbytesc(out10, in5, iskip, nbits, 8, 5)
+    end if
+    do i = 1, 10
+       ! print '(z2.2)', out10(i)
+       if (mod(i, 2) .gt. 0) then
+          if (ichar(out10(i)) .ne. in5(int(i/2) + 1)) stop 51;
+       else
+          if (ichar(out10(i)) .ne. 0) stop 50;
+       endif
+    end do
   end do
 
   print *, 'Testing sbytec() with iskip of 1...'
